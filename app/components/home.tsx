c4aab613fb97a04a3a968b3fd14e20d74ce0d6be
YangHao White <waitKey1@outlook.com>
2024-03-07 22:53:01 +0000

    add a function

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -173,11 +173,12 @@ export function useLoadData() {
   const config = useAppConfig();
 
   var api: ClientApi;
-  if (config.modelConfig.model === "gemini-pro") {
-    api = new ClientApi(ModelProvider.GeminiPro);
-  } else {
-    api = new ClientApi(ModelProvider.GPT);
-  }
+  // if (config.modelConfig.model === "gemini-pro") {
+  //   api = new ClientApi(ModelProvider.GeminiPro);
+  // } else {
+  //   api = new ClientApi(ModelProvider.GPT);
+  // }
+  api = new ClientApi(ModelProvider.GPT);
   useEffect(() => {
     (async () => {
       const models = await api.llm.models();
